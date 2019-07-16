910a62146759ffa6355af8b8fc2d37d365d1a257
FunctionJoo <joojy0914@gmail.com>
2019-07-16 11:09:40 +0900

    make study path

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ app.use(express.static('public'))
 const path = require('path');
 const router = express.Router();
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/main.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 
+// index 접속
+app.use('/', router);
 
-// app.use('/', router);
+// 스터디 파일 공유 폴더 접속 가능 경로 생성
+// 그냥 study 하위파일은 전체 경로에서 open 가능하게 했는데, 추후 app.js 경로 공부나 설정 해보시고 싶을때는 말씀 해주시고 수정 하시면 됩니다.
+app.use('/study', express.static('study'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
