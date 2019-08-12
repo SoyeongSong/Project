4ee3b67d297c78d71bcab6cb432176af09ed88d5
eeaeoon <leehoon0820@naver.com>
2019-08-12 16:13:28 +0900

    commit

--- study/0812/jaehoon.java
+++ study/0812/jaehoon.java
@@ -1,23 +1,17 @@
 
 public class jaehoon {
     public static void main(String[] args) {
-        int i, j;
-        int k = 1;
-        for (i = 1; i < 8; i++) {
-            for (j = 0; j < 5; j++) {
-                if (i == 1 || i == 7) {
-                    System.out.printf("~\t");
-                } else {
-                    if (j == 0 || j == 4) {
-                        System.out.printf("!\t");
-                    } else {
-                        System.out.printf(k + "\t");
-                        k += 5;
-                    }
-                }
-            }
-            k = i;
-            System.out.printf("\n");
-        }
-    }
+        String str1 = new String("This is really not immutable!!"); 
+        String str2 = str1;
+        boolean retVal; 
+        int length;
+        double d = 102939939.939; 
+        retVal = str1.equals(str2); 
+        System.out.printf("Returned Value = " + retVal); 
+        System.out.printf("String Length is : " + str1.length()); 
+        System.out.printf("String comparing is : " + str1.compareTo(str2)); 
+        System.out.printf("String concat is : " + str1.concat(str2)); 
+        System.out.printf("String replaceAll is : " + str1.replaceAll("is", "??")); 
+        System.out.printf("String substring is : " + str1.substring(10)); 
+        System.out.printf("Return Value : " + String.valueOf(d)); 
 }
