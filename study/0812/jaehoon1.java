f8b8f5f4b2feb3780f5a8e6788b78702a95e8ded
eeaeoon <leehoon0820@naver.com>
2019-08-13 10:11:22 +0900

    commit

---
+++ study/0812/jaehoon1.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class jaehoon {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+        hmap.put(12, "Chaitanya");
+        hmap.put(2, "Rahul");
+        hmap.put(7, "Singh");
+        hmap.put(49, "Ajeet");
+        hmap.put(3, "Anuj");
+        Set set = hmap.entrySet();
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry) iterator.next();
+            System.out.println("key:" + mentry.getKey() + " & Value: " + mentry.getValue());
+        }
+        for (Entry<Integer, String> entry : hmap.entrySet()) {
+            System.out.println("key:" + entry.getKey() + " & Value: " + entry.getValue());
+        }
+        for (Integer key : hmap.keySet()) {
+            System.out.println("key:" + key + " & Value: " + hmap.get(key));
+        }
+
+    }
+}
